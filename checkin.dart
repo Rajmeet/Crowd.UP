aa2ad9da115da210071ee8e86ab22b417bd06e6c
Darkrai23 <Camboning23@gmail.com>
2020-09-20 03:26:19 -0400

    Add files via upload
    Obviously unfinished. Just uploading so y'all can see where I'm at.

---
+++ checkin.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+import 'home_page.dart'
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  static const String _title = 'Check-In';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: MyStatelessWidget(),
+      ),
+    );
+  }
+}
+
+
+class MyStatelessWidget extends StatelessWidget {
+  MyStatelessWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text('Would you like to check-in?', style: TextStyle(fontSize: 28)),
+          const SizedBox(height: 30),
+          RaisedButton(
+            onPressed: () {
+              // Navigator.pull(context)// should go forward to event main screen
+              print('Thank you for attending!');
+            },
+            child: const Text('Yes', style: TextStyle(fontSize: 20)),
+            color: Colors.green,
+          ),
+          const SizedBox(height: 30),
+          RaisedButton(
+            onPressed: () {
+            //  Navigator.pop(context);  // This should go back to the list of events
+            },
+            child: const Text('No', style: TextStyle(fontSize: 20)),
+            color: Colors.red,
+          ),
+        ],
+      ),
+    );
+  }
+}
